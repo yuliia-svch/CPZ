2236d9cbe40f29397f2b548f66861ec39e7b447e
euzLIsnY <euzlisny@email.com>
2021-09-20 15:42:24 +0300

    initial commit

---
+++ SavchenkoYuliia.RobotChallenge/DistanceHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlberdaRoman.RobotChallange
+{
+	public class DistanceHelper
+	{
+		public static int FindDistance(Position a, Position b)
+		{
+			return Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
+		}
+	}
+}
---
+++ SavchenkoYuliia.RobotChallenge/SavchenkoYuliiaAlgorithm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robot.Common;
+
+namespace SavchenkoYuliia.RobotChallenge
+{
+    public class SavchenkoYuliiaAlgorithm : IRobotAlgorithm
+    {
+        public string Author {
+            get { return "Yuliia Savchenko"; }
+        }
+
+        public RobotCommand DoStep(IList<Robot.Common.Robot> robots, int robotToMoveIndex, Map map)
+        {
+            var myRobot = robots[robotToMoveIndex];
+                if ((movingRobot.Energy > 500) && (robots.Count < map.Stations.Count))
+            {
+            return new CreateNewRobotCommand();
+            }
+
+            Position stationPosition = FindNearestFreeStation(robots[robotToMoveIndex], map, robots);
+
+            if (stationPosition == null)
+            return null;
+            if (stationPosition == movingRobot.Position)
+            return new CollectEnergyCommand();
+            else
+            {
+            return new MoveCommand(){NewPosition = stationPosition };
+            }
+            
+        }
+    }
+}
